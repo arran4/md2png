3dc4e1aba611a24b746b0351deeb4163c70cf8fb
arran4 <111667+arran4@users.noreply.github.com>
2026-09-09 04:36:05 +0000

    build: pin generate.go to immutable revision with pointer parameter test fix

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package md2png
 
-//go:generate go run github.com/arran4/go-subcommand/cmd/gosubc@v0.0.29 generate --timestamp=false --project-provenance=false --force --replace-template gosubc_overlay
+//go:generate go run github.com/arran4/go-subcommand/cmd/gosubc@v0.0.30-0.20260909021348-caa7cff8ac3c generate --timestamp=false --project-provenance=false --force --replace-template gosubc_overlay
